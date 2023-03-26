d2444b4d24c269946eabf191b4ec8ecb8bc26da6
Volodymyr <labish.vova@gmail.com>
2023-03-26 18:10:34 +0300

    add binding

---
+++ app/src/main/java/com/buffersolve/cuton/CutOnActivity.kt
@@ -0,0 +1,20 @@
+package com.buffersolve.cuton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.buffersolve.cuton.databinding.ActivityCutonBinding
+
+class CutOnActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityCutonBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCutonBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        
+        
+        
+    }
+}
